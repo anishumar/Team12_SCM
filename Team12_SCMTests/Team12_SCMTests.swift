2fb984ce21cfabf0ac60623aad9d53ff7c916b10
anishumar <anishumar786@gmail.com>
2024-12-02 11:58:08 +0530

    Initial Commit

---
+++ Team12_SCMTests/Team12_SCMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team12_SCMTests.swift
+//  Team12_SCMTests
+//
+//  Created by Anish Umar on 02/12/24.
+//
+
+import Testing
+@testable import Team12_SCM
+
+struct Team12_SCMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
